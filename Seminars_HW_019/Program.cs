416c5fe758459ae9d8c45321885893ffe86da1fb
Dudyrev <koska.du@mail.ru>
2022-12-24 14:53:21 +0400

    Tasks 19, 21, 23

---
+++ Seminars_HW_019/Program.cs
@@ -0,0 +1,27 @@
+﻿/*Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+Выполнить с помощью числовых операций (целочисленное деление, остаток от деления).
+14212 -> нет
+12821 -> да
+23432 -> да*/
+
+Console.Write("Введите число для проверки на полиндромность: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+bool IsPalindrome(int x)
+{
+    x = Math.Abs(x);
+
+    if (x % 10 == 0 && x != 0) 
+    return false;
+
+    int revNumber = 0;
+
+    while (x > revNumber)
+    {
+        revNumber = revNumber * 10 + x % 10;
+        x = x / 10;
+    }
+    return x == revNumber || x == revNumber / 10;
+}
+
+Console.WriteLine(IsPalindrome(number));
---
+++ Seminars_HW_023/Program.cs
@@ -0,0 +1,23 @@
+﻿/*Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.*/
+
+
+void Numb(int n1)
+{
+    int counter = 1;
+    while (counter <= n1)
+    {
+        Console.WriteLine($"{counter, 5} {Math.Pow(counter, 3), 5}");
+        counter++;
+    }
+}
+
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+while (number < 1)
+{
+    Console.WriteLine("Введите положительное, целое число");
+    number = Convert.ToInt32(Console.ReadLine());
+}
+
+Numb(number);
