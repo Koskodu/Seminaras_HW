89e8c9af43a21f0495c75cabbb3b4963eccdf74d
Dudyrev <koska.du@mail.ru>
2023-02-01 13:49:20 +0400

    Tasks 64,66,68

---
+++ Seminars_HW_066/Program.cs
@@ -0,0 +1,25 @@
+﻿/*Задача 66: Задайте значения M и N. 
+Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N. 
+Выполнить с помощью рекурсии.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30*/
+
+Console.Write("Введите первое число : ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число : ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int SumNatural(int digit1, int digit2)
+{
+    if (digit1 < digit2)
+    {
+    return digit1 == digit2? digit2 : digit1 + SumNatural(digit1 + 1, digit2);
+    }
+    else
+    {
+    return digit1 == digit2? digit2 : digit2 + SumNatural(digit1, digit2 + 1);
+    }
+}
+
+int natSum = SumNatural(m, n);
+Console.WriteLine($"Сумму натуральных элементов в промежутке от {m} до {n} = {natSum}");
---
+++ Seminars_HW_068/Program.cs
@@ -0,0 +1,24 @@
+﻿/*Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+Даны два неотрицательных числа m и n.
+m = 2, n = 3 -> A(m,n) = 9
+m = 3, n = 2 -> A(m,n) = 29*/
+
+
+Console.Write("Введите первое число : ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число : ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int AkFunction(int val1, int val2)
+{
+    if (val1 == 0)
+        return val2 + 1;
+    else
+    if ((val1 != 0) && (val2 == 0))
+        return AkFunction(val1 - 1, 1);
+    else
+        return AkFunction(val1 - 1, AkFunction(val1, val2 - 1));
+}
+
+int res = AkFunction(m, n);
+Console.WriteLine($"{res}");
